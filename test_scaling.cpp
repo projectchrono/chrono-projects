c33cd1c3e315dc2216089c5409bff90587e3cc8d
Radu Serban <serban@engr.wisc.edu>
2014-07-09 00:09:59 -0500

    Replace tabs with spaces (no functional changes).

--- test_scaling.cpp
+++ test_scaling.cpp
@@ -50,28 +50,28 @@ double hThickness = 0.1;   // wall thickness
 
 void CreateObjects(ChSystemParallel* system)
 {
-	// Create a material for the ball mixture
-	ChSharedPtr<ChMaterialSurfaceDEM> ballMixMat;
-	ballMixMat = ChSharedPtr<ChMaterialSurfaceDEM>(new ChMaterialSurfaceDEM);
-	ballMixMat->SetYoungModulus(1e8f);
-	ballMixMat->SetFriction(0.4f);
-	ballMixMat->SetDissipationFactor(0.1f);
+  // Create a material for the ball mixture
+  ChSharedPtr<ChMaterialSurfaceDEM> ballMixMat;
+  ballMixMat = ChSharedPtr<ChMaterialSurfaceDEM>(new ChMaterialSurfaceDEM);
+  ballMixMat->SetYoungModulus(1e8f);
+  ballMixMat->SetFriction(0.4f);
+  ballMixMat->SetDissipationFactor(0.1f);
 
-	// Create a mixture entirely made out of spheres
-	utils::Generator gen(system);
+  // Create a mixture entirely made out of spheres
+  utils::Generator gen(system);
 
-	utils::MixtureIngredientPtr& m1 = gen.AddMixtureIngredient(utils::SPHERE, 1.0);
-	m1->setDefaultMaterialDEM(ballMixMat);
-	m1->setDefaultDensity(density);
-	m1->setDefaultSize(radius);
+  utils::MixtureIngredientPtr& m1 = gen.AddMixtureIngredient(utils::SPHERE, 1.0);
+  m1->setDefaultMaterialDEM(ballMixMat);
+  m1->setDefaultDensity(density);
+  m1->setDefaultSize(radius);
 
-	// Generate the objects
-	gen.createObjectsBox(utils::POISSON_DISK,
-	                     2.01 * radius,
-	                     ChVector<>(0, 0, 2.5),
-	                     ChVector<>(0.8 * hDimX, 0.8 * hDimY, 2));
+  // Generate the objects
+  gen.createObjectsBox(utils::POISSON_DISK,
+                       2.01 * radius,
+                       ChVector<>(0, 0, 2.5),
+                       ChVector<>(0.8 * hDimX, 0.8 * hDimY, 2));
 
-	cout << "Number bodies generated: " << gen.getTotalNumBodies() << endl;
+  cout << "Number bodies generated: " << gen.getTotalNumBodies() << endl;
 }
 
 // =======================================================================
@@ -80,84 +80,84 @@ void CreateObjects(ChSystemParallel* system)
 int main(int argc, char* argv[])
 {
 
-	// Create system
-	ChSystemParallelDEM* msystem = new ChSystemParallelDEM();
+  // Create system
+  ChSystemParallelDEM* msystem = new ChSystemParallelDEM();
 
-	// Set number of threads.
-	int max_threads = msystem->GetParallelThreadNumber();
-	if (threads > max_threads)
-		threads = max_threads;
-	msystem->SetParallelThreadNumber(threads);
-	omp_set_num_threads(threads);
+  // Set number of threads.
+  int max_threads = msystem->GetParallelThreadNumber();
+  if (threads > max_threads)
+    threads = max_threads;
+  msystem->SetParallelThreadNumber(threads);
+  omp_set_num_threads(threads);
 
-	// Set gravitational acceleration
-	msystem->Set_G_acc(ChVector<>(0, 0, gravity));
+  // Set gravitational acceleration
+  msystem->Set_G_acc(ChVector<>(0, 0, gravity));
 
-	// Edit system settings
-	msystem->SetMaxiter(max_iteration);
-	msystem->SetIterLCPmaxItersSpeed(max_iteration);
-	msystem->SetTol(1e-3);
-	msystem->SetTolSpeeds(1e-3);
-	msystem->SetStep(time_step);
+  // Edit system settings
+  msystem->SetMaxiter(max_iteration);
+  msystem->SetIterLCPmaxItersSpeed(max_iteration);
+  msystem->SetTol(1e-3);
+  msystem->SetTolSpeeds(1e-3);
+  msystem->SetStep(time_step);
 
-	((ChCollisionSystemParallel*) msystem->GetCollisionSystem())->setBinsPerAxis(I3(10, 10, 10));
-	((ChCollisionSystemParallel*) msystem->GetCollisionSystem())->setBodyPerBin(100, 50);
+  ((ChCollisionSystemParallel*) msystem->GetCollisionSystem())->setBinsPerAxis(I3(10, 10, 10));
+  ((ChCollisionSystemParallel*) msystem->GetCollisionSystem())->setBodyPerBin(100, 50);
 
-	((ChCollisionSystemParallel*) msystem->GetCollisionSystem())->ChangeNarrowphase(new ChCNarrowphaseR);
+  ((ChCollisionSystemParallel*) msystem->GetCollisionSystem())->ChangeNarrowphase(new ChCNarrowphaseR);
 
-	// Create container bin.
-	ChSharedPtr<ChMaterialSurfaceDEM> binMat;
-	binMat = ChSharedPtr<ChMaterialSurfaceDEM>(new ChMaterialSurfaceDEM);
-	binMat->SetYoungModulus(2e6f);
-	binMat->SetFriction(0.4f);
-	binMat->SetDissipationFactor(0.6f);
+  // Create container bin.
+  ChSharedPtr<ChMaterialSurfaceDEM> binMat;
+  binMat = ChSharedPtr<ChMaterialSurfaceDEM>(new ChMaterialSurfaceDEM);
+  binMat->SetYoungModulus(2e6f);
+  binMat->SetFriction(0.4f);
+  binMat->SetDissipationFactor(0.6f);
 
-	utils::CreateBoxContainerDEM(msystem, binId, binMat, ChVector<>(hDimX, hDimY, hDimZ), hThickness);
+  utils::CreateBoxContainerDEM(msystem, binId, binMat, ChVector<>(hDimX, hDimY, hDimZ), hThickness);
 
-	// Create the granular material
-	CreateObjects(msystem);
-
-
-	// Number of steps
-	int num_frames1 = std::ceil(measure_interval / time_step);
-	int num_frames2 = std::ceil((time_end - 2 * measure_interval) / time_step);
-
-	// Perform the simulation
-	double timeA = 0;
-	int ncA = 0;
-
-	for (int i = 0; i < num_frames1; i++) {
-		msystem->DoStepDynamics(time_step);
-		timeA += msystem->GetTimerStep();
-		ncA += msystem->GetNcontacts();
-	}
-
-	for (int i = 0; i < num_frames2; i++) {
-		msystem->DoStepDynamics(time_step);
-	}
-
-	double timeB = 0;
-	int ncB = 0;
-
-	for (int i = 0; i < num_frames1; i++) {
-		msystem->DoStepDynamics(time_step);
-		timeB += msystem->GetTimerStep();
-		ncB += msystem->GetNcontacts();
-	}
-
-	// Final stats
-	cout << "==================================" << endl;
-	cout << "Number of threads: " << threads << endl;
-	cout << "Number of bodies: " << msystem->Get_bodylist()->size() << endl;
-	cout << "Number of frames: " << num_frames1 << endl;
-	cout << "Simulation time A: " << timeA << endl;
-	cout << "Average num. contacts A: " << double(ncA) / num_frames1 << endl;
-	cout << "Simulation time B: " << timeB << endl;
-	cout << "Average num. contacts B: " << double(ncB) / num_frames1 << endl;
+  // Create the granular material
+  CreateObjects(msystem);
 
 
-	msystem->gpu_data_manager->system_timer.PrintReport();
+  // Number of steps
+  int num_frames1 = std::ceil(measure_interval / time_step);
+  int num_frames2 = std::ceil((time_end - 2 * measure_interval) / time_step);
 
-	return 0;
+  // Perform the simulation
+  double timeA = 0;
+  int ncA = 0;
+
+  for (int i = 0; i < num_frames1; i++) {
+    msystem->DoStepDynamics(time_step);
+    timeA += msystem->GetTimerStep();
+    ncA += msystem->GetNcontacts();
+  }
+
+  for (int i = 0; i < num_frames2; i++) {
+    msystem->DoStepDynamics(time_step);
+  }
+
+  double timeB = 0;
+  int ncB = 0;
+
+  for (int i = 0; i < num_frames1; i++) {
+    msystem->DoStepDynamics(time_step);
+    timeB += msystem->GetTimerStep();
+    ncB += msystem->GetNcontacts();
+  }
+
+  // Final stats
+  cout << "==================================" << endl;
+  cout << "Number of threads: " << threads << endl;
+  cout << "Number of bodies: " << msystem->Get_bodylist()->size() << endl;
+  cout << "Number of frames: " << num_frames1 << endl;
+  cout << "Simulation time A: " << timeA << endl;
+  cout << "Average num. contacts A: " << double(ncA) / num_frames1 << endl;
+  cout << "Simulation time B: " << timeB << endl;
+  cout << "Average num. contacts B: " << double(ncB) / num_frames1 << endl;
+
+
+  msystem->gpu_data_manager->system_timer.PrintReport();
+
+  return 0;
 }
 
--- test_utils.cpp
+++ test_utils.cpp
@@ -20,139 +20,139 @@ const std::string data_folder = "../TEST/";
 void writePoints(const std::string          filename,
                  const utils::PointVectorD& points)
 {
-	utils::CSV_writer csv(" ");
-	for (int i = 0; i < points.size(); i++) {
-		csv << points[i] << std::endl;
-	}
-	char file[100];
-	csv.write_to_file(data_folder + filename);
-	std::cout << "Wrote " << points.size() << " points to " << data_folder + filename << std::endl;
+  utils::CSV_writer csv(" ");
+  for (int i = 0; i < points.size(); i++) {
+    csv << points[i] << std::endl;
+  }
+  char file[100];
+  csv.write_to_file(data_folder + filename);
+  std::cout << "Wrote " << points.size() << " points to " << data_folder + filename << std::endl;
 }
 
 void checkSamplers()
 {
-	utils::PointVectorD  points;
+  utils::PointVectorD  points;
 
-	// Grid sampler
-	utils::GridSampler<> gs(ChVector<>(0.1, 0.2, 0.3));
-	
-	points = gs.SampleBox(ChVector<>(2, 0, 0), ChVector<>(0, 2, 1));
-	writePoints("GS_rectangleX.out", points);
+  // Grid sampler
+  utils::GridSampler<> gs(ChVector<>(0.1, 0.2, 0.3));
+  
+  points = gs.SampleBox(ChVector<>(2, 0, 0), ChVector<>(0, 2, 1));
+  writePoints("GS_rectangleX.out", points);
 
-	points = gs.SampleBox(ChVector<>(0, 3, 0), ChVector<>(1, 0, 2));
-	writePoints("GS_rectangleY.out", points);
+  points = gs.SampleBox(ChVector<>(0, 3, 0), ChVector<>(1, 0, 2));
+  writePoints("GS_rectangleY.out", points);
 
-	points = gs.SampleBox(ChVector<>(0, 0, 2), ChVector<>(1, 2, 0));
-	writePoints("GS_rectangleZ.out", points);
+  points = gs.SampleBox(ChVector<>(0, 0, 2), ChVector<>(1, 2, 0));
+  writePoints("GS_rectangleZ.out", points);
 
-	points = gs.SampleBox(ChVector<>(-1, -2, -3), ChVector<>(1, 1.5, 0.5));
-	writePoints("GS_box.out", points);
+  points = gs.SampleBox(ChVector<>(-1, -2, -3), ChVector<>(1, 1.5, 0.5));
+  writePoints("GS_box.out", points);
 
-	points = gs.SampleSphere(ChVector<>(1, 1, 1), 1);
-	writePoints("GS_sphere.out", points);
+  points = gs.SampleSphere(ChVector<>(1, 1, 1), 1);
+  writePoints("GS_sphere.out", points);
 
-	points = gs.SampleCylinderX(ChVector<>(1, 0, -1), 2, 0);
-	writePoints("GS_circleX.out", points);
+  points = gs.SampleCylinderX(ChVector<>(1, 0, -1), 2, 0);
+  writePoints("GS_circleX.out", points);
 
-	points = gs.SampleCylinderY(ChVector<>(1, 0, -1), 2, 0);
-	writePoints("GS_circleY.out", points);
+  points = gs.SampleCylinderY(ChVector<>(1, 0, -1), 2, 0);
+  writePoints("GS_circleY.out", points);
 
-	points = gs.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0);
-	writePoints("GS_circleZ.out", points);
+  points = gs.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0);
+  writePoints("GS_circleZ.out", points);
 
-	points = gs.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0.5);
-	writePoints("GS_cylinderZ.out", points);
+  points = gs.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0.5);
+  writePoints("GS_cylinderZ.out", points);
 
 
-	// Poisson Disk sampler
-	utils::PDSampler<> pd(0.2);
+  // Poisson Disk sampler
+  utils::PDSampler<> pd(0.2);
 
-	points = pd.SampleBox(ChVector<>(2, 0, 0), ChVector<>(0, 2, 1));
-	writePoints("PD_rectangleX.out", points);
+  points = pd.SampleBox(ChVector<>(2, 0, 0), ChVector<>(0, 2, 1));
+  writePoints("PD_rectangleX.out", points);
 
-	points = pd.SampleBox(ChVector<>(0, 3, 0), ChVector<>(1, 0, 2));
-	writePoints("PD_rectangleY.out", points);
+  points = pd.SampleBox(ChVector<>(0, 3, 0), ChVector<>(1, 0, 2));
+  writePoints("PD_rectangleY.out", points);
 
-	points = pd.SampleBox(ChVector<>(0, 0, 2), ChVector<>(1, 2, 0));
-	writePoints("PD_rectangleZ.out", points);
+  points = pd.SampleBox(ChVector<>(0, 0, 2), ChVector<>(1, 2, 0));
+  writePoints("PD_rectangleZ.out", points);
 
-	points = pd.SampleCylinderX(ChVector<>(1, 0, -1), 2, 0);
-	writePoints("PD_circleX.out", points);
+  points = pd.SampleCylinderX(ChVector<>(1, 0, -1), 2, 0);
+  writePoints("PD_circleX.out", points);
 
-	points = pd.SampleCylinderY(ChVector<>(1, 0, -1), 2, 0);
-	writePoints("PD_circleY.out", points);
+  points = pd.SampleCylinderY(ChVector<>(1, 0, -1), 2, 0);
+  writePoints("PD_circleY.out", points);
 
-	points = pd.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0);
-	writePoints("PD_circleZ.out", points);
+  points = pd.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0);
+  writePoints("PD_circleZ.out", points);
 
-	points = pd.SampleBox(ChVector<>(-1, -2, -3), ChVector<>(1, 1.5, 1));
-	writePoints("PD_box.out", points);
+  points = pd.SampleBox(ChVector<>(-1, -2, -3), ChVector<>(1, 1.5, 1));
+  writePoints("PD_box.out", points);
 
-	points = pd.SampleSphere(ChVector<>(1, 1, 1), 1);
-	writePoints("PD_sphere.out", points);
+  points = pd.SampleSphere(ChVector<>(1, 1, 1), 1);
+  writePoints("PD_sphere.out", points);
 
-	points = pd.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0.5);
-	writePoints("PD_cylinderZ.out", points);
+  points = pd.SampleCylinderZ(ChVector<>(1, 0, -1), 2, 0.5);
+  writePoints("PD_cylinderZ.out", points);
 }
 
 void checkGenerators()
 {
-	char filename[100];
+  char filename[100];
 
 
-	// Parameters for the falling ball
-	int             ballId = 100;
-	double          radius = .1;
-	double          density = 2000;
-	double          volume = (4.0/3) * CH_C_PI * radius * radius * radius;
-	double          mass = density * volume;
-	ChVector<>      inertia = 0.4 * mass * radius * radius * ChVector<>(1,1,1);
-	ChVector<>      init_vel(0, 0, 0);
+  // Parameters for the falling ball
+  int             ballId = 100;
+  double          radius = .1;
+  double          density = 2000;
+  double          volume = (4.0/3) * CH_C_PI * radius * radius * radius;
+  double          mass = density * volume;
+  ChVector<>      inertia = 0.4 * mass * radius * radius * ChVector<>(1,1,1);
+  ChVector<>      init_vel(0, 0, 0);
 
-	// Create system
-	ChSystemParallelDEM* msystem = new ChSystemParallelDEM();
+  // Create system
+  ChSystemParallelDEM* msystem = new ChSystemParallelDEM();
 
-	// Create a material for the balls
-	ChSharedPtr<ChMaterialSurfaceDEM> ballMat;
-	ballMat = ChSharedPtr<ChMaterialSurfaceDEM>(new ChMaterialSurfaceDEM);
-	ballMat->SetYoungModulus(5e4f);
-	ballMat->SetFriction(0.4f);
-	ballMat->SetDissipationFactor(0.6f);
-
-
-	// CHECK GENERATOR
-	utils::Generator gen(msystem);
-
-	utils::MixtureIngredientPtr& m1 = gen.AddMixtureIngredient(utils::SPHERE, 0.1);
-	m1->setDefaultSize(radius);
-	m1->setDistributionDensity(1000, 600, 500, 3000);
-
-	utils::MixtureIngredientPtr& m2 = gen.AddMixtureIngredient(utils::BOX, 0.25);
-	m2->setDefaultSize(radius);
-	m2->setDefaultDensity(1000);
-
-	utils::MixtureIngredientPtr& m3 = gen.AddMixtureIngredient(utils::ELLIPSOID, 0.15);
-	m3->setDefaultSize(radius);
-	m3->setDefaultDensity(2000);
-
-	gen.createObjectsBox(utils::POISSON_DISK, radius, ChVector<>(0, 0, 2), ChVector<>(3, 3, 0));
-
-	std::cout << "Total mass: "<< gen.getTotalMass() << "  Total volume: " << gen.getTotalVolume() << std::endl;
-
-	gen.writeObjectInfo(data_folder + "body_info.dat");
+  // Create a material for the balls
+  ChSharedPtr<ChMaterialSurfaceDEM> ballMat;
+  ballMat = ChSharedPtr<ChMaterialSurfaceDEM>(new ChMaterialSurfaceDEM);
+  ballMat->SetYoungModulus(5e4f);
+  ballMat->SetFriction(0.4f);
+  ballMat->SetDissipationFactor(0.6f);
 
 
-	// CHECK CSV WRITER
-	sprintf(filename, "%sdata_000.dat", data_folder.c_str());
-	utils::WriteShapesRender(msystem, filename);
+  // CHECK GENERATOR
+  utils::Generator gen(msystem);
+
+  utils::MixtureIngredientPtr& m1 = gen.AddMixtureIngredient(utils::SPHERE, 0.1);
+  m1->setDefaultSize(radius);
+  m1->setDistributionDensity(1000, 600, 500, 3000);
+
+  utils::MixtureIngredientPtr& m2 = gen.AddMixtureIngredient(utils::BOX, 0.25);
+  m2->setDefaultSize(radius);
+  m2->setDefaultDensity(1000);
+
+  utils::MixtureIngredientPtr& m3 = gen.AddMixtureIngredient(utils::ELLIPSOID, 0.15);
+  m3->setDefaultSize(radius);
+  m3->setDefaultDensity(2000);
+
+  gen.createObjectsBox(utils::POISSON_DISK, radius, ChVector<>(0, 0, 2), ChVector<>(3, 3, 0));
+
+  std::cout << "Total mass: "<< gen.getTotalMass() << "  Total volume: " << gen.getTotalVolume() << std::endl;
+
+  gen.writeObjectInfo(data_folder + "body_info.dat");
+
+
+  // CHECK CSV WRITER
+  sprintf(filename, "%sdata_000.dat", data_folder.c_str());
+  utils::WriteShapesRender(msystem, filename);
 }
 
 
 int main(int argc, char* argv[])
 {
-	checkSamplers();
-	//checkGenerators();
+  checkSamplers();
+  //checkGenerators();
 
-	return 0;
+  return 0;
 }
 
