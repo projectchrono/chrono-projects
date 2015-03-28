3636a4b879c8a745aa5a044b9e99c549151d7c6e
Radu Serban <serban@engr.wisc.edu>
2015-03-27 18:46:21 -0700

    Modify the HMMWV demo to allow selection of the terrain type (rigid or granular).

--- hmmwv.cpp
+++ hmmwv.cpp
@@ -12,6 +12,7 @@
 #include "chrono_utils/ChUtilsVehicle.h"
 #include "chrono_utils/ChUtilsGeometry.h"
 #include "chrono_utils/ChUtilsCreators.h"
+#include "chrono_utils/ChUtilsGenerators.h"
 #include "chrono_utils/ChUtilsInputOutput.h"
 
 //#undef CHRONO_PARALLEL_HAS_OPENGL
@@ -32,10 +33,10 @@ using std::endl;
 // Specification of the vehicle model
 // -----------------------------------------------------------------------------
 
-enum WheelType {CYLINDRICAL, LUGGED};
+enum WheelType { CYLINDRICAL, LUGGED };
 
 // Type of wheel/tire (controls both contact and visualization)
-WheelType wheel_type = LUGGED;
+WheelType wheel_type = CYLINDRICAL;
 
 // JSON files for vehicle model (using different wheel visualization meshes)
 std::string vehicle_file_cyl("hmmwv/vehicle/HMMWV_Vehicle_simple.json");
@@ -45,8 +46,41 @@ std::string vehicle_file_lug("hmmwv/vehicle/HMMWV_Vehicle_simple_lugged.json");
 std::string simplepowertrain_file("hmmwv/powertrain/HMMWV_SimplePowertrain.json");
 
 // Initial vehicle position and orientation
-ChVector<> initLoc(0, 0, 1.0);
+ChVector<> initLoc(-3.0, 0, 0.75);
 ChQuaternion<> initRot(1, 0, 0, 0);
+
+// Coefficient of friction
+float mu_t = 0.8;
+
+// -----------------------------------------------------------------------------
+// Specification of the terrain
+// -----------------------------------------------------------------------------
+
+enum TerrainType { RIGID, GRANULAR };
+
+// Type of terrain
+TerrainType terrain_type = RIGID;
+
+// Control visibility of containing bin walls
+bool visible_walls = false;
+
+// Dimensions
+double hdimX = 5.5;
+double hdimY = 1.75;
+double hdimZ = 0.5;
+double hthick = 0.25;
+
+// Parameters for granular material
+int Id_g = 100;
+double r_g = 0.02;
+double rho_g = 2500;
+double vol_g = (4.0 / 3) * CH_C_PI * r_g * r_g * r_g;
+double mass_g = rho_g * vol_g;
+ChVector<> inertia_g = 0.4 * mass_g * r_g * r_g * ChVector<>(1, 1, 1);
+
+float mu_g = 0.8;
+
+int num_particles = 1000;
 
 // -----------------------------------------------------------------------------
 // Simulation parameters
@@ -58,14 +92,19 @@ int threads = 100;
 // Perform dynamic tuning of number of threads?
 bool thread_tuning = true;
 
-// Simulation duration.
-double time_end = 5;
+// Total simulation duration.
+double time_end = 7;
+
+// Duration of the "hold time" (vehicle chassis fixed and no driver inputs).
+// This can be used to allow the granular material to settle.
+double time_hold = 2;
 
 // Solver parameters
 double time_step = 1e-3;
 
-double tolerance = 1e-1;
+double tolerance = 0.1;
 
+int max_iteration_bilateral = 100;
 int max_iteration_normal = 0;
 int max_iteration_sliding = 200;
 int max_iteration_spinning = 0;
@@ -80,24 +119,32 @@ const std::string pov_dir = out_dir + "/POVRAY";
 
 int out_fps = 60;
 
-// Continuous loop (only if OpenGL available)
-bool loop = false;
-
 // =============================================================================
 
 // Callback class for providing driver inputs.
 class MyDriverInputs : public utils::DriverInputsCallback {
  public:
+  MyDriverInputs(double delay) : m_delay(delay) {}
+
   virtual void onCallback(double time, double& throttle, double& steering, double& braking) {
     throttle = 0;
     steering = 0;
     braking = 0;
 
-    if (time > 0.5)
+    double eff_time = time - m_delay;
+
+    // Do not generate any driver inputs for a duration equal to m_delay.
+    if (eff_time < 0)
+      return;
+
+    if (eff_time > 0.5)
       throttle = 1.0;
-    else if (time > 0.25)
-      throttle = 4 * (time - 0.25);
+    else if (eff_time > 0.25)
+      throttle = 4 * (eff_time - 0.25);
   }
+
+ private:
+  double m_delay;
 };
 
 // Callback class for specifying rigid tire contact model.
@@ -109,7 +156,7 @@ class MyCylindricalTire : public utils::TireContactCallback {
     wheelBody->GetCollisionModel()->AddCylinder(0.46, 0.46, width / 2);
     wheelBody->GetCollisionModel()->BuildModel();
 
-    wheelBody->GetMaterialSurface()->SetFriction(0.6f);
+    wheelBody->GetMaterialSurface()->SetFriction(mu_t);
   }
 };
 
@@ -144,7 +191,7 @@ class MyLuggedTire : public utils::TireContactCallback {
 
     coll_model->BuildModel();
 
-    wheelBody->GetMaterialSurface()->SetFriction(0.8f);
+    wheelBody->GetMaterialSurface()->SetFriction(mu_t);
   }
 
  private:
@@ -176,7 +223,7 @@ public:
     utils::AddCylinderGeometry(wheelBody.get_ptr(), 0.223, 0.126);
     wheelBody->GetCollisionModel()->BuildModel();
 
-    wheelBody->GetMaterialSurface()->SetFriction(0.8f);
+    wheelBody->GetMaterialSurface()->SetFriction(mu_t);
   }
 
 private:
@@ -184,6 +231,36 @@ private:
   geometry::ChTriangleMeshConnected lugged_mesh;
 };
 
+
+// =============================================================================
+
+double CreateParticles(ChSystem* system) {
+  // Create a material
+  ChSharedPtr<ChMaterialSurface> mat_g(new ChMaterialSurface);
+  mat_g->SetFriction(mu_g);
+
+  // Create a particle generator and a mixture entirely made out of spheres
+  utils::Generator gen(system);
+  utils::MixtureIngredientPtr& m1 = gen.AddMixtureIngredient(utils::SPHERE, 1.0);
+  m1->setDefaultMaterialDVI(mat_g);
+  m1->setDefaultDensity(rho_g);
+  m1->setDefaultSize(r_g);
+
+  // Set starting value for body identifiers
+  gen.setBodyIdentifier(Id_g);
+
+  // Create particles in layers until reaching the desired number of particles
+  double r = 1.01 * r_g;
+  ChVector<> hdims(hdimX - r, hdimY - r, 0);
+  ChVector<> center(0, 0, 2 * r);
+
+  while (gen.getTotalNumBodies() < num_particles) {
+    gen.createObjectsBox(utils::POISSON_DISK, 2 * r, center, hdims);
+    center.z += 2 * r;
+  }
+
+  return center.z;
+}
 
 // =============================================================================
 
@@ -232,6 +309,7 @@ int main(int argc, char* argv[]) {
   system->GetSettings()->solver.tolerance = tolerance;
 
   system->GetSettings()->solver.solver_mode = SLIDING;
+  system->GetSettings()->solver.max_iteration_bilateral = max_iteration_bilateral;
   system->GetSettings()->solver.max_iteration_normal = max_iteration_normal;
   system->GetSettings()->solver.max_iteration_sliding = max_iteration_sliding;
   system->GetSettings()->solver.max_iteration_spinning = max_iteration_spinning;
@@ -241,6 +319,49 @@ int main(int argc, char* argv[]) {
 
   system->GetSettings()->collision.bins_per_axis = I3(10, 10, 10);
 
+  // -------------------
+  // Create the terrain.
+  // -------------------
+
+  // Ground body
+  ChSharedPtr<ChBody> ground = ChSharedPtr<ChBody>(new ChBody(new collision::ChCollisionModelParallel));
+  ground->SetIdentifier(-1);
+  ground->SetBodyFixed(true);
+  ground->SetCollide(true);
+
+  ground->GetMaterialSurface()->SetFriction(mu_g);
+
+  ground->GetCollisionModel()->ClearModel();
+
+  // Bottom box
+  utils::AddBoxGeometry(ground.get_ptr(), ChVector<>(hdimX, hdimY, hthick), ChVector<>(0, 0, -hthick),
+                        ChQuaternion<>(1, 0, 0, 0), true);
+  if (terrain_type == GRANULAR) {
+    // Front box
+    utils::AddBoxGeometry(ground.get_ptr(), ChVector<>(hthick, hdimY, hdimZ + hthick),
+                          ChVector<>(hdimX + hthick, 0, hdimZ - hthick), ChQuaternion<>(1, 0, 0, 0), visible_walls);
+    // Rear box
+    utils::AddBoxGeometry(ground.get_ptr(), ChVector<>(hthick, hdimY, hdimZ + hthick),
+                          ChVector<>(-hdimX - hthick, 0, hdimZ - hthick), ChQuaternion<>(1, 0, 0, 0), visible_walls);
+    // Left box
+    utils::AddBoxGeometry(ground.get_ptr(), ChVector<>(hdimX, hthick, hdimZ + hthick),
+                          ChVector<>(0, hdimY + hthick, hdimZ - hthick), ChQuaternion<>(1, 0, 0, 0), visible_walls);
+    // Right box
+    utils::AddBoxGeometry(ground.get_ptr(), ChVector<>(hdimX, hthick, hdimZ + hthick),
+                          ChVector<>(0, -hdimY - hthick, hdimZ - hthick), ChQuaternion<>(1, 0, 0, 0), visible_walls);
+  }
+
+  ground->GetCollisionModel()->BuildModel();
+
+  system->AddBody(ground);
+
+  // Create the granular material.
+  double vertical_offset = 0;
+
+  if (terrain_type == GRANULAR) {
+    vertical_offset = CreateParticles(system);
+  }
+
   // -----------------------------------------
   // Create and initialize the vehicle system.
   // -----------------------------------------
@@ -248,6 +369,8 @@ int main(int argc, char* argv[]) {
   utils::VehicleSystem* vehicle;
   utils::TireContactCallback* tire_cb;
 
+  // Create the vehicle assembly and the callback object for tire contact
+  // according to the specified type of tire/wheel.
   switch (wheel_type) {
     case CYLINDRICAL: {
       vehicle = new utils::VehicleSystem(system, vehicle_file_cyl, simplepowertrain_file);
@@ -255,41 +378,22 @@ int main(int argc, char* argv[]) {
     } break;
     case LUGGED: {
       vehicle = new utils::VehicleSystem(system, vehicle_file_lug, simplepowertrain_file);
-      tire_cb = new MyLuggedTire_vis();
+      tire_cb = new MyLuggedTire();
     } break;
   }
 
   vehicle->SetTireContactCallback(tire_cb);
 
-  MyDriverInputs driver_cb;
+  // Set the callback object for driver inputs. Pass the hold time as a delay in
+  // generating driver inputs.
+  MyDriverInputs driver_cb(time_hold);
   vehicle->SetDriverInputsCallback(&driver_cb);
 
-  vehicle->Initialize(initLoc, initRot);
+  // Initially, fix the chassis (will be released after time_hold).
+  vehicle->GetVehicle()->GetChassis()->SetBodyFixed(true);
 
-  // ------------------------------------------------
-  // Create the ground body and set contact geometry.
-  // ------------------------------------------------
-  double hdimX = 100;
-  double hdimY = 100;
-  double hdimZ = 5;
-
-  ChSharedPtr<ChBody> ground = ChSharedPtr<ChBody>(new ChBody(new collision::ChCollisionModelParallel));
-  ground->SetIdentifier(-1);
-  ground->SetBodyFixed(true);
-  ground->SetCollide(true);
-  ground->SetPos(ChVector<>(0, 0, -hdimZ));
-
-  ground->GetMaterialSurface()->SetFriction(0.8f);
-
-  ground->GetCollisionModel()->ClearModel();
-  ground->GetCollisionModel()->AddBox(hdimX, hdimY, hdimZ);
-  ground->GetCollisionModel()->BuildModel();
-
-  ChSharedPtr<ChBoxShape> box_ground(new ChBoxShape);
-  box_ground->GetBoxGeometry().Size = ChVector<>(hdimX, hdimY, hdimZ);
-  ground->AddAsset(box_ground);
-
-  system->AddBody(ground);
+  // Initialize the vehicle at a height above the terrain.
+  vehicle->Initialize(initLoc + ChVector<>(0, 0, vertical_offset), initRot);
 
 // -----------------------
 // Perform the simulation.
@@ -300,15 +404,7 @@ int main(int argc, char* argv[]) {
   opengl::ChOpenGLWindow& gl_window = opengl::ChOpenGLWindow::getInstance();
   gl_window.Initialize(1280, 720, "HMMWV", system);
   gl_window.SetCamera(ChVector<>(0, -10, 0), ChVector<>(0, 0, 0), ChVector<>(0, 0, 1));
-
-  // Let the OpenGL manager run the simulation until interrupted.
-  // NOTE: we need to add a user callback to the OpenGL library to give back
-  // control to the user before advancing the simulation by one step (in this
-  // case to call the vehicle update function).
-  if (loop) {
-    gl_window.StartDrawLoop(time_step);
-    return 0;
-  }
+  gl_window.SetRenderMode(opengl::WIREFRAME);
 #endif
 
   // Run simulation for specified time.
@@ -322,6 +418,7 @@ int main(int argc, char* argv[]) {
   int num_contacts = 0;
 
   while (time < time_end) {
+    // If enabled, output data for PovRay postprocessing.
     if (povray_output && sim_frame == next_out_frame) {
       char filename[100];
       sprintf(filename, "%s/data_%03d.dat", pov_dir.c_str(), out_frame + 1);
@@ -337,6 +434,10 @@ int main(int argc, char* argv[]) {
       next_out_frame += out_steps;
       num_contacts = 0;
     }
+
+    // Release the vehicle chassis at the end of the hold time.
+    if (vehicle->GetVehicle()->GetChassis()->GetBodyFixed() && time > time_hold)
+      vehicle->GetVehicle()->GetChassis()->SetBodyFixed(false);
 
     // Update vehicle
     vehicle->Update(time);
