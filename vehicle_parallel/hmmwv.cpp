a4adbb6c72027717b6e2e1559307dfe1b5550ad0
Radu Serban <serban@engr.wisc.edu>
2015-11-12 23:33:03 +0100

    Use new Chrono::Vehicle function to set the path to the default location for the Chrono::Vehicle data files

--- vehicle_parallel/hmmwv.cpp
+++ vehicle_parallel/hmmwv.cpp
@@ -309,8 +309,9 @@ double CreateParticles(ChSystem* system) {
 // =============================================================================
 
 int main(int argc, char* argv[]) {
-    // Set path to Chrono data directory
+    // Set path to Chrono and Chrono::Vehicle data directories
     SetChronoDataPath(CHRONO_DATA_DIR);
+    vehicle::SetDataPath(CHRONO_VEHICLE_DATA_DIR);
 
     // --------------------------
     // Create output directories.
