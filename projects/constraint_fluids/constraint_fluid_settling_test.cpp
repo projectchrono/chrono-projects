d0f1bbaebc703e5c6a5f99a7ae5b525381264e17
Hammad Mazhar <hammad.maz@gmail.com>
2016-08-28 01:08:13 -0700

    cleanup and update fluid tank settling simulation

--- projects/constraint_fluids/constraint_fluid_settling_test.cpp
+++ projects/constraint_fluids/constraint_fluid_settling_test.cpp
@@ -18,33 +18,35 @@
 
 using namespace chrono;
 using namespace chrono::collision;
+
 double timestep = 1e-3;
 ChFluidContainer* fluid_container;
 std::ofstream ofile;
-real tolerance = 1e-8;
-std::string tolerance_str = "1e-8";
 
-int out_frame = 0;
-int next_out_frame = 0;
+real tolerances[] = { 100, 10, 1, 1e-1, 1e-3, 1e-5, 1e-8 };
+std::string tol_string[] = { "100", "10", "1", "1e-1", "1e-3", "1e-5", "1e-8" };
+
+real tolerance = tolerances[2];
+std::string tolerance_str = tol_string[2];
 
 void WriteData(ChSystemParallelDVI* msystem, uint i) {
-	double iters = ((ChIterativeSolverParallel*)(msystem->GetSolverSpeed()))->GetTotalIterations();
+	int iters = ((ChIterativeSolverParallel*)(msystem->GetSolverSpeed()))->GetTotalIterations();
 	const std::vector<double>& vhist = ((ChIterativeSolverParallel*)(msystem->GetSolverSpeed()))->GetViolationHistory();
 	const std::vector<double>& dhist = ((ChIterativeSolverParallel*)(msystem->GetSolverSpeed()))->GetDeltalambdaHistory();
-	double residual = vhist.size() > 0 ? vhist.back() : 0.0;
-	double dlambda = dhist.size() > 0 ? dhist.back() : 0.0;
+	real residual = vhist.size() > 0 ? vhist.back() : 0.0;
+	real dlambda = dhist.size() > 0 ? dhist.back() : 0.0;
 
 	std::vector<real> density, pressure;
-	std::vector<real3> force;
-	std::vector<chrono::real> force_length;
-	std::vector<chrono::real> marker_height;
+	//std::vector<real3> force;
+	//std::vector<chrono::real> force_length;
+	//std::vector<chrono::real> marker_height;
 
 	fluid_container->GetFluidDensity(density);
 	fluid_container->GetFluidPressure(pressure);
-	fluid_container->GetFluidForce(force);
+	//fluid_container->GetFluidForce(force);
 
-	force_length.resize(density.size());
-	marker_height.resize(density.size());
+	//force_length.resize(density.size());
+	//marker_height.resize(density.size());
 
 	real avg_density = 0;
 	real avg_pressure = 0;
@@ -52,25 +54,25 @@ void WriteData(ChSystemParallelDVI* msystem, uint i) {
 	for (int i = 0; i < density.size(); i++) {
 		avg_density += density[i];
 		avg_pressure += pressure[i];
-		marker_height[i] = msystem->data_manager->host_data.pos_3dof[i].z;
-		force_length[i] = Length(force[i]);
+		//marker_height[i] = msystem->data_manager->host_data.pos_3dof[i].z;
+		//force_length[i] = Length(force[i]);
 		KE += .5 * fluid_container->mass * Pow(Length(msystem->data_manager->host_data.vel_3dof[i]), 2);
 	}
 	avg_density = avg_density / density.size();
 	avg_pressure = avg_pressure / pressure.size();
+	double SOLVER = msystem->GetTimerSolver();
+	printf("%d %d %f %f avg D: %f, avg P:  %f, KE: %f SolverTime: %f\n", i, iters, residual, dlambda, avg_density, avg_pressure,
+		KE, SOLVER);
 
-	printf("%d %d %f %f avg D, avg P, KE: %f %f %f \n", i, int(iters), residual, dlambda, avg_density, avg_pressure,
-		KE);
-
-	ofile << i << " " << int(iters) << " " << residual << " " << dlambda << " " << avg_density << " " << avg_pressure
-		<< " " << KE << std::endl;
+	ofile << i << " " << iters << " " << residual << " " << dlambda << " " << avg_density << " " << avg_pressure
+		<< " " << KE <<" "<<SOLVER<<std::endl;
 }
 
 void AddContainer(ChSystemParallelDVI* sys) {
 	// Create a common material
 	auto mat = std::make_shared<ChMaterialSurface>();
-	mat->SetFriction(0.4f);
-	ChVector<> hdim(.55, .6, .55);
+	mat->SetFriction(1.0);
+	ChVector<> hdim(.175, .175, .65);
 	utils::CreateBoxContainer(sys, 0, mat, hdim, 0.05, Vector(0, 0, -hdim.z - 0.05 * 4), QUNIT, true, false, true,
 		true);
 }
@@ -113,15 +115,9 @@ void AddFluid(ChSystemParallelDVI* sys) {
 	double dist = fluid_container->kernel_radius * .9;
 	utils::HCPSampler<> sampler(dist);
 	vol = dist * dist * dist * .6;
-#if 0
-	utils::Generator::PointVector points = sampler.SampleBox(ChVector<>(0, 0, 0), radius);
-	// vol = 4.0 / 3.0 * CH_C_PI * pow(radius, 3) / real(points.size());
 
-#else
-	ChVector<> hdim(.5, .5, .5);
+	ChVector<> hdim(.15, .15, .6);
 	utils::Generator::PointVector points = sampler.SampleBox(ChVector<>(0, 0, 0), hdim);
-	// vol = hdim.x * hdim.y * hdim.z / real(points.size());
-#endif
 
 	pos_fluid.resize(points.size());
 	vel_fluid.resize(points.size());
@@ -136,20 +132,18 @@ void AddFluid(ChSystemParallelDVI* sys) {
 	fluid_container->AddBodies(pos_fluid, vel_fluid);
 }
 
-double tolerances[] = { 100, 10, 1, 1e-1, 1e-3, 1e-5, 1e-8 };
-std::string tol_string[] = { "100", "10", "1", "1e-1", "1e-3", "1e-5", "1e-8" };
+
 // -----------------------------------------------------------------------------
 // Create the system, specify simulation parameters, and run simulation loop.
 // -----------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
-	double gravity = 9.81;
 	double time_end = 10;
-	bool useAPGD = false;
-	uint max_iteration = 10000;
+	int solver_type = 2;
+	uint max_iteration = 1000;
 	if (argc == 3) {
 		tolerance = tolerances[atoi(argv[1])];
 		tolerance_str = tol_string[atoi(argv[1])];
-		useAPGD = atoi(argv[2]);
+		solver_type = atoi(argv[2]);
 	}
 
 	// Create system
@@ -157,7 +151,7 @@ int main(int argc, char* argv[]) {
 
 	ChSystemParallelDVI msystem;
 	// Set gravitational acceleration
-	msystem.Set_G_acc(ChVector<>(0, 0, -gravity));
+	msystem.Set_G_acc(ChVector<>(0, 0, -9.81));
 
 	// Set solver parameters
 	msystem.GetSettings()->solver.solver_mode = SLIDING;
@@ -171,11 +165,28 @@ int main(int argc, char* argv[]) {
 	msystem.GetSettings()->solver.contact_recovery_speed = 100000;
 	msystem.GetSettings()->solver.cache_step_length = true;
 	msystem.GetSettings()->min_threads = 8;
-	if (useAPGD) {
-		msystem.ChangeSolverType(APGD);
-	}
-	else {
+
+	switch (solver_type) {
+	case 0:
 		msystem.ChangeSolverType(BB);
+		ofile.open("data_fluid_test/fluid_container_data_BB_" + tolerance_str + ".txt");
+		break;
+	case 1:
+		msystem.ChangeSolverType(APGD);
+		ofile.open("data_fluid_test/fluid_container_data_APGD_" + tolerance_str + ".txt");
+		break;
+	case 2:
+		msystem.ChangeSolverType(JACOBI);
+		ofile.open("data_fluid_test/fluid_container_data_JACOBI_" + tolerance_str + ".txt");
+		break;
+	case 3:
+		msystem.ChangeSolverType(GAUSS_SEIDEL);
+		ofile.open("data_fluid_test/fluid_container_data_GAUSS_SEIDEL_" + tolerance_str + ".txt");
+		break;
+	default:
+		msystem.ChangeSolverType(APGD);
+		ofile.open("data_fluid_test/fluid_container_data_APGD_" + tolerance_str + ".txt");
+		break;
 	}
 
 	msystem.GetSettings()->collision.narrowphase_algorithm = NARROWPHASE_HYBRID_MPR;
@@ -192,19 +203,17 @@ int main(int argc, char* argv[]) {
 
 	// Perform the simulation
 	// ----------------------
-	ofile.open("data_fluid_test_" + tolerance_str + "/fluid_container_data_" + tolerance_str + ".txt");
+	
 #if 0
 	opengl::ChOpenGLWindow& gl_window = opengl::ChOpenGLWindow::getInstance();
-	gl_window.Initialize(1280, 720, "fluidDVI", &msystem);
+	gl_window.Initialize(1280, 720, "fluid_settling_test", &msystem);
 	gl_window.SetCamera(ChVector<>(0, -2, 0), ChVector<>(0, 0, 0), ChVector<>(0, 0, 1), .2);
 	gl_window.Pause();
-	// Uncomment the following two lines for the OpenGL manager to automatically
-	// run the simulation in an infinite loop.
-	// gl_window.StartDrawLoop(timestep);
-	// return 0;
+
 	while (true) {
 		if (gl_window.Active()) {
 			if (gl_window.DoStepDynamics(timestep)) {
+
 			}
 			gl_window.Render();
 		}
@@ -213,34 +222,18 @@ int main(int argc, char* argv[]) {
 		}
 	}
 #else
-	int out_fps = 60;
-	double time = 0;
-	int sim_frame = 0;
-
 	double exec_time = 0;
-	int num_contacts = 0;
-	int out_steps = std::ceil((1.0 / timestep) / out_fps);
 	int num_steps = time_end / timestep;
 
 	//=========================================================================================================
-	int file = 0;
 	for (int i = 0; i < num_steps; i++) {
 		msystem.DoStepDynamics(timestep);
-		std::cout << i << std::endl;
 		WriteData(&msystem, i);
-		if (i == next_out_frame) {
-			std::cout << "write: " << out_frame << std::endl;
-			out_frame++;
-			next_out_frame += out_steps;
-		}
-
 		exec_time += msystem.GetTimerStep();
 	}
 	ofile << exec_time << std::endl;
 	printf("Execution Time: %f \n", exec_time);
 #endif
-
 	ofile.close();
-
 	return 0;
 }
