0155b82ed2b1acca923b45958023aeafa3405ba4
Ruochun Zhang <rzhang294@euler.wacc.wisc.edu>
2021-02-04 22:16:33 -0600

    Update test_GPU_milsettle to comply with new C::GPU

--- projects/gpu_tests/test_GPU_milsettle.cpp
+++ projects/gpu_tests/test_GPU_milsettle.cpp
@@ -9,7 +9,7 @@
 // http://projectchrono.org/license-chrono.txt.
 //
 // =============================================================================
-// Authors: Conlain Kelly
+// Authors: Conlain Kelly, Ruochun Zhang
 // =============================================================================
 // Chrono::Granular simulation of up to one million spherical particles
 // settling in a box in order to measure run time.
@@ -21,13 +21,12 @@
 
 #include "chrono/core/ChTimer.h"
 #include "chrono/utils/ChUtilsSamplers.h"
-#include "chrono_granular/ChGranularData.h"
-#include "chrono_granular/api/ChApiGranularChrono.h"
-#include "chrono_granular/physics/ChGranular.h"
+#include "chrono_gpu/ChGpuData.h"
+#include "chrono_gpu/physics/ChSystemGpu.h"
 #include "chrono_thirdparty/filesystem/path.h"
 
 using namespace chrono;
-using namespace chrono::granular;
+using namespace chrono::gpu;
 
 std::string output_prefix = "../results";
 
@@ -44,8 +43,8 @@ float normalDampS2W = 1000;
 float adhesion_ratio_s2w = 0.f;
 float timestep = 1e-4;
 
-GRAN_OUTPUT_MODE write_mode = GRAN_OUTPUT_MODE::BINARY;
-GRAN_VERBOSITY verbose = GRAN_VERBOSITY::INFO;
+CHGPU_OUTPUT_MODE write_mode = CHGPU_OUTPUT_MODE::BINARY;
+CHGPU_VERBOSITY verbose = CHGPU_VERBOSITY::INFO;
 float cohesion_ratio = 0;
 
 // -----------------------------------------------------------------------------
@@ -53,39 +52,37 @@ float cohesion_ratio = 0;
 // -----------------------------------------------------------------------------
 double run_test(float box_size_X, float box_size_Y, float box_size_Z) {
     // Setup simulation
-    ChSystemGranularSMC gran_system(ballRadius, ballDensity, make_float3(box_size_X, box_size_Y, box_size_Z));
-    gran_system.set_K_n_SPH2SPH(normStiffness_S2S);
-    gran_system.set_K_n_SPH2WALL(normStiffness_S2W);
-    gran_system.set_Gamma_n_SPH2SPH(normalDampS2S);
-    gran_system.set_Gamma_n_SPH2WALL(normalDampS2W);
+    ChSystemGpu gpu_system(ballRadius, ballDensity, make_float3(box_size_X, box_size_Y, box_size_Z));
+    gpu_system.SetKn_SPH2SPH(normStiffness_S2S);
+    gpu_system.SetKn_SPH2WALL(normStiffness_S2W);
+    gpu_system.SetGn_SPH2SPH(normalDampS2S);
+    gpu_system.SetGn_SPH2WALL(normalDampS2W);
 
-    gran_system.set_Cohesion_ratio(cohesion_ratio);
-    gran_system.set_Adhesion_ratio_S2W(adhesion_ratio_s2w);
-    gran_system.set_gravitational_acceleration(0.f, 0.f, grav_acceleration);
-    gran_system.setOutputMode(write_mode);
+    gpu_system.SetCohesionRatio(cohesion_ratio);
+    gpu_system.SetAdhesionRatio_SPH2WALL(adhesion_ratio_s2w);
+    gpu_system.SetGravitationalAcceleration(ChVector<>(0.f, 0.f, grav_acceleration));
+    gpu_system.SetOutputMode(write_mode);
 
     // Fill the bottom half with material
     chrono::utils::HCPSampler<float> sampler(2.4 * ballRadius);  // Add epsilon
     ChVector<float> center(0, 0, -.25 * box_size_Z);
     ChVector<float> hdims(box_size_X / 2 - ballRadius, box_size_X / 2 - ballRadius, box_size_Z / 4 - ballRadius);
     std::vector<ChVector<float>> body_points = sampler.SampleBox(center, hdims);
-    ChGranularSMC_API apiSMC;
-    apiSMC.setGranSystem(&gran_system);
-    apiSMC.setElemsPositions(body_points);
+    gpu_system.SetParticlePositions(body_points);
 
     filesystem::create_directory(filesystem::path(output_prefix));
 
-    gran_system.set_BD_Fixed(true);
-    gran_system.set_friction_mode(GRAN_FRICTION_MODE::FRICTIONLESS);
-    gran_system.set_timeIntegrator(GRAN_TIME_INTEGRATOR::EXTENDED_TAYLOR);
-    gran_system.setVerbose(verbose);
+    gpu_system.SetBDFixed(true);
+    gpu_system.SetFrictionMode(CHGPU_FRICTION_MODE::FRICTIONLESS);
+    gpu_system.SetTimeIntegrator(CHGPU_TIME_INTEGRATOR::EXTENDED_TAYLOR);
+    gpu_system.SetVerbosity(verbose);
 
-    gran_system.set_fixed_stepSize(timestep);
+    gpu_system.SetFixedStepSize(timestep);
     ChTimer<double> timer;
 
     // Run wavetank experiment and time it
     timer.start();
-    gran_system.initialize();
+    gpu_system.Initialize();
     int fps = 50;
     // assume we run for at least one frame
     float frame_step = 1.0f / fps;
@@ -94,19 +91,18 @@ double run_test(float box_size_X, float box_size_Y, float box_size_Z) {
 
     // Run settling experiments
     while (curr_time < timeEnd) {
-        gran_system.advance_simulation(frame_step);
+        gpu_system.AdvanceSimulation(frame_step);
         curr_time += frame_step;
         printf("rendering frame %u\n", currframe);
         char filename[100];
         sprintf(filename, "%s/step%06d", output_prefix.c_str(), currframe++);
-        gran_system.writeFile(filename);
+        gpu_system.WriteFile(filename);
     }
     timer.stop();
     return timer.GetTimeSeconds();
 }
 
 int main(int argc, char* argv[]) {
-    granular::SetDataPath(std::string(PROJECTS_DATA_DIR) + "granular/");
     if (argc != 2) {
         std::cout << "usage: " + std::string(argv[0]) + " <results_log_file>" << std::endl;
     }
