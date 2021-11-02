c2122fb47b8d9333410b6457e9baeb5ac4b20ef1
Asher Elmquist <amelmquist@wisc.edu>
2021-11-02 16:45:30 -0400

    add blended terrain textures, add trees, add mile markers

--- projects/synchrono_highway/demo_SYN_highway.cpp
+++ projects/synchrono_highway/demo_SYN_highway.cpp
@@ -20,6 +20,7 @@
 #include "chrono/core/ChStream.h"
 #include "chrono/utils/ChUtilsInputOutput.h"
 
+#include <irrlicht.h>
 #include "chrono_models/vehicle/hmmwv/HMMWV.h"
 #include "chrono_vehicle/ChConfigVehicle.h"
 #include "chrono_vehicle/ChVehicleModelData.h"
@@ -27,13 +28,12 @@
 #include "chrono_vehicle/driver/ChIrrGuiDriver.h"
 #include "chrono_vehicle/terrain/RigidTerrain.h"
 #include "chrono_vehicle/wheeled_vehicle/utils/ChWheeledVehicleIrrApp.h"
-#include <irrlicht.h>
 
 #include "chrono_thirdparty/filesystem/path.h"
 
-#include "chrono_sensor/sensors/ChCameraSensor.h"
 #include "chrono_sensor/ChSensorManager.h"
 #include "chrono_sensor/filters/ChFilterVisualize.h"
+#include "chrono_sensor/sensors/ChCameraSensor.h"
 #include "chrono_sensor/utils/ChVisualMaterialUtils.h"
 #include "chrono_thirdparty/cxxopts/ChCLI.h"
 #include "chrono_vehicle/driver/ChPathFollowerDriver.h"
@@ -58,9 +58,15 @@ using namespace chrono::synchrono;
 // Initial vehicle location and orientation
 // ChVector<> initLoc(-788, -195, -1);
 // ChVector<> initLoc(3990.1, -1200.3124, .75); //middle-ish of oval highway
-ChVector<> initLoc(3982.1, -12837, .75);  // extreme-ish of oval highway
+// ChVector<> initLoc(3991.5, 0.0, .75);
+ChVector<> initLoc(4011.5, -345, .75);  // near mile marker
 // ChQuaternion<> initRot(1, 0, 0, 0);
-ChQuaternion<> initRot = Q_from_AngZ(-CH_C_PI_2);
+// ChQuaternion<> initRot = Q_from_AngZ(-CH_C_PI_2);
+ChQuaternion<> initRot = Q_from_AngZ(CH_C_PI_2);
+
+ChVector<> driver_eye(-.2, .4, .95);
+// ChVector<> driver_eye(1.0, .4, .95);
+ChQuaternion<> driver_view_direction = Q_from_AngAxis(0, {1, 0, 0});
 
 enum DriverMode { HUMAN, AUTONOMOUS };
 DriverMode driver_mode = AUTONOMOUS;
@@ -119,7 +125,7 @@ bool sensor_save = false;
 bool sensor_vis = true;
 
 /// Speedometer image data
-irr::core::position2d<irr::s32> sm_center(300,200);
+irr::core::position2d<irr::s32> sm_center(300, 200);
 double sm_needle = 140;
 
 std::string demo_data_path = std::string(STRINGIFY(HIGHWAY_DATA_DIR));
@@ -157,7 +163,7 @@ int main(int argc, char* argv[]) {
     if (use_fullscreen) {
         image_width = fullscreen_image_width;
         image_height = fullscreen_image_height;
-	    cam_fov = 1.608f;
+        cam_fov = 1.608f;
     }
     bool disable_joystick = cli.GetAsType<bool>("nojoystick");
     bool lbj_joystick = cli.GetAsType<bool>("lbj");
@@ -204,7 +210,7 @@ int main(int argc, char* argv[]) {
     switch (terrain_model) {
         case RigidTerrain::PatchType::BOX:
             patch = terrain.AddPatch(patch_mat, ChVector<>(0, 0, 0), ChVector<>(0, 0, 1), terrainLength, terrainWidth,
-                                     2, false, 1, true);
+                                     2, false, 1, false);
             // patch->SetTexture(vehicle::GetDataFile("terrain/textures/tile4.jpg"), 200, 200);
             break;
         case RigidTerrain::PatchType::HEIGHT_MAP:
@@ -219,31 +225,97 @@ int main(int argc, char* argv[]) {
             break;
     }
 
-    // auto ground_body = patch->GetGroundBody();
-    // auto visual_asset = std::dynamic_pointer_cast<ChVisualization>(ground_body->GetAssets()[0]);
-    // auto vis_mat = chrono_types::make_shared<ChVisualMaterial>();
-    // vis_mat->SetKdTexture(vehicle::GetDataFile("terrain/textures/grass.jpg"));
-    // vis_mat->SetSpecularColor({.0f, .0f, .0f});
-    // vis_mat->SetRoughness(1.f);
-    // vis_mat->SetUseSpecularWorkflow(false);
-    // visual_asset->material_list.push_back(vis_mat);
-
     terrain.Initialize();
 
-    // // add terrain as visual only
-    auto mmesh = chrono_types::make_shared<ChTriangleMeshConnected>();
-    mmesh->LoadWavefrontMesh(demo_data_path + "/Environments/Iowa/oval_highway.obj", false, true);
-    mmesh->Transform(ChVector<>(0, 0, 0), ChMatrix33<>(1));  // scale to a different size
 
+    //add terrain with weighted textures
+    auto terrain_mesh = chrono_types::make_shared<ChTriangleMeshConnected>();
+    terrain_mesh->LoadWavefrontMesh(demo_data_path + "/Environments/Iowa/terrain.obj", false, true);
+    terrain_mesh->Transform(ChVector<>(0, 0, 0), ChMatrix33<>(1));  // scale to a different size
+    auto terrain_shape = chrono_types::make_shared<ChTriangleMeshShape>();
+    terrain_shape->SetMesh(terrain_mesh);
+    terrain_shape->SetName("terrain");
+    terrain_shape->SetStatic(true);
+
+    auto vis_mat2 = chrono_types::make_shared<ChVisualMaterial>();
+    vis_mat2->SetKdTexture(demo_data_path + "/Environments/Iowa/Gravel/GroundGravel017_COL_1K.jpg");
+    vis_mat2->SetRoughnessTexture(demo_data_path + "/Environments/Iowa/Gravel/GroundGravel017_ROUGH_1K.png");
+    vis_mat2->SetNormalMapTexture(demo_data_path + "/Environments/Iowa/Gravel/GroundGravel017_NRM_1K.jpg");
+    vis_mat2->SetWeightTexture(demo_data_path + "/Environments/Iowa/weight_gravel.png");
+    vis_mat2->SetSpecularColor({.0f, .0f, .0f});
+    vis_mat2->SetTextureScale({500.0,500.0,1.0});
+    vis_mat2->SetRoughness(1.f);
+    vis_mat2->SetUseSpecularWorkflow(false);
+    terrain_shape->material_list.push_back(vis_mat2);
+
+    auto vis_mat1 = chrono_types::make_shared<ChVisualMaterial>();
+    // vis_mat1->SetKdTexture(demo_data_path + "/Environments/Iowa/Grass/T_grass_a_d.png"));
+    vis_mat1->SetKdTexture(GetChronoDataFile("vehicle/terrain/textures/grass.jpg"));
+    vis_mat1->SetWeightTexture(demo_data_path + "/Environments/Iowa/weight_grass.png");
+    vis_mat1->SetTextureScale({20.0,20.0,1.0});
+    vis_mat1->SetSpecularColor({.0f, .0f, .0f});
+    vis_mat1->SetRoughness(1.f);
+    vis_mat1->SetUseSpecularWorkflow(false);
+    terrain_shape->material_list.push_back(vis_mat1);
+
+    auto terrain_body = chrono_types::make_shared<ChBody>();
+    terrain_body->SetPos({0,0,-.01});
+    terrain_body->AddAsset(terrain_shape);
+    terrain_body->SetBodyFixed(true);
+    vehicle.GetSystem()->Add(terrain_body);
+
+
+    std::vector<std::string> environment_meshes = {
+        "/Environments/Iowa/mile_markers_inner.obj", "/Environments/Iowa/mile_markers_outer.obj",
+        "/Environments/Iowa/oval_highway.obj"};
+    std::vector<ChVector<>> offsets = {{0, 0, -128.22}, {0, 0, 0.0}, {0, 0, 0.01}};
+
+    for (int i = 0; i < environment_meshes.size(); i++) {  // auto file_name : environment_meshes) {
+        // additional environment assets
+        auto trimesh = chrono_types::make_shared<ChTriangleMeshConnected>();
+        trimesh->LoadWavefrontMesh(demo_data_path + environment_meshes[i], false, true);
+        trimesh->Transform(ChVector<>(0, 0, 0), ChMatrix33<>(1));  // scale to a different size
+        auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
+        trimesh_shape->SetMesh(trimesh);
+        trimesh_shape->SetName(environment_meshes[i]);
+        trimesh_shape->SetStatic(true);
+        auto mesh_body = chrono_types::make_shared<ChBody>();
+        mesh_body->SetPos(offsets[i]);
+        mesh_body->AddAsset(trimesh_shape);
+        mesh_body->SetBodyFixed(true);
+        vehicle.GetSystem()->Add(mesh_body);
+    }
+
+    // add in corn for testing
+
+    auto trimesh = chrono_types::make_shared<ChTriangleMeshConnected>();
+    trimesh->LoadWavefrontMesh(demo_data_path + "/Environments/Iowa/tree_01.obj", false, true);
+    trimesh->Transform(ChVector<>(0, 0, 0), ChMatrix33<>(1));  // scale to a different size
     auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
-    trimesh_shape->SetMesh(mmesh);
-    trimesh_shape->SetName("highway");
+    trimesh_shape->SetMesh(trimesh);
+    trimesh_shape->SetName("Tree");
+    trimesh_shape->SetStatic(true);
+    trimesh_shape->SetScale({1,1,1});
 
-    auto mesh_body = chrono_types::make_shared<ChBody>();
-    mesh_body->SetPos({0, 0, .01});
-    mesh_body->AddAsset(trimesh_shape);
-    mesh_body->SetBodyFixed(true);
-    vehicle.GetSystem()->Add(mesh_body);
+    double x_step = 100;
+    double y_step = 200;
+
+    double x_start = initLoc.x()-60;
+    double y_start = initLoc.y()-7500;
+
+    int x_count = 2;
+    int y_count = 75;
+
+    for (int i =0; i<x_count; i++){
+        for(int j=0; j<y_count; j++){
+            auto mesh_body = chrono_types::make_shared<ChBody>();
+            mesh_body->SetPos({i*x_step+x_start,j*y_step+y_start+.2*(i%3),0.0});
+            // mesh_body->SetRot(Q_from_AngZ(CH_C_PI_2));
+            mesh_body->AddAsset(trimesh_shape);
+            mesh_body->SetBodyFixed(true);
+            vehicle.GetSystem()->Add(mesh_body);
+        }
+    }
 
     // -----------------
     // Initialize output
@@ -258,33 +330,32 @@ int main(int argc, char* argv[]) {
     // Create the driver system
     // ------------------------
 
-    ChWheeledVehicleIrrApp app(&vehicle, L"Highway Demo",irr::core::dimension2d<irr::u32>(600, 400));
+    ChWheeledVehicleIrrApp app(&vehicle, L"Highway Demo", irr::core::dimension2d<irr::u32>(600, 400));
     ChRealtimeStepTimer realtime_timer;
     /*
-    SPEEDOMETER: we want to use the irrlicht app to display the speedometer, but calling endscene would update the entire (massive) scenario.
-    In order to do so, we first have to clean app the Irrlichr app. Once we delete the node, we remove all cached meshes and textures.
-    The order is important, otherwise meshes are re-cached!!
+    SPEEDOMETER: we want to use the irrlicht app to display the speedometer, but calling endscene would update the
+    entire (massive) scenario. In order to do so, we first have to clean app the Irrlichr app. Once we delete the node,
+    we remove all cached meshes and textures. The order is important, otherwise meshes are re-cached!!
     */
     irr::scene::ISceneNode* mnode = app.GetContainer();
     mnode->remove();
-    irr::scene::IMeshCache *cache = app.GetDevice()->getSceneManager()->getMeshCache();
-    cache->clear ();
+    irr::scene::IMeshCache* cache = app.GetDevice()->getSceneManager()->getMeshCache();
+    cache->clear();
     app.GetVideoDriver()->removeAllTextures();
-    
-    
+
 #ifdef CHRONO_IRRKLANG
-    GetLog() << "USING IRRKLANG" << "\n\n";
+    GetLog() << "USING IRRKLANG"
+             << "\n\n";
     ChCSLSoundEngine soundEng(&vehicle);
 #endif
-
 
     // Create the interactive driver system
     // ChCSLDriver driver(vehicle);
     // driver = chrono_types::make_shared<ChCSLDriver>(vehicle);
     auto IGdriver = chrono_types::make_shared<ChIrrGuiDriver>(app);
     IGdriver->SetButtonCallback(6, &customButtonCallback);
-    IGdriver->SetJoystickAxes(ChIrrGuiDriver::JoystickAxes::AXIS_Z, ChIrrGuiDriver::JoystickAxes::AXIS_R, 
-	ChIrrGuiDriver::JoystickAxes::AXIS_X, ChIrrGuiDriver::JoystickAxes::NONE);
+    IGdriver->SetJoystickAxes(ChIrrGuiDriver::JoystickAxes::AXIS_Z, ChIrrGuiDriver::JoystickAxes::AXIS_R,
+                              ChIrrGuiDriver::JoystickAxes::AXIS_X, ChIrrGuiDriver::JoystickAxes::NONE);
     IGdriver->Initialize();
 
     double mph_to_ms = 0.44704;
@@ -302,7 +373,7 @@ int main(int argc, char* argv[]) {
     } else {
         driver_mode = AUTONOMOUS;
         std::cout << "Using path follower driver\n";
-    }    
+    }
 
     // ---------------
     // Simulation loop
@@ -330,6 +401,8 @@ int main(int argc, char* argv[]) {
     b.mode = BackgroundMode::ENVIRONMENT_MAP;
     b.env_tex = GetChronoDataFile("sensor/textures/sunflowers_4k.hdr");
     manager->scene->SetBackground(b);
+    manager->scene->SetFogScatteringFromDistance(2000.0);
+    manager->scene->SetFogColor({.8, .8, .8});
 
     // ------------------------------------------------
     // Create a camera and add it to the sensor manager
@@ -353,11 +426,11 @@ int main(int argc, char* argv[]) {
     // Create a second camera and add it to the sensor manager
     // -------------------------------------------------------
     auto cam2 = chrono_types::make_shared<ChCameraSensor>(
-        vehicle.GetChassisBody(),                                               // body camera is attached to
-        frame_rate,                                                             // update rate in Hz
-        chrono::ChFrame<double>({-.2, .4, .95}, Q_from_AngAxis(0, {1, 0, 0})),  // offset pose
-        image_width,                                                            // image width
-        image_height,                                                           // image height
+        vehicle.GetChassisBody(),                                    // body camera is attached to
+        frame_rate,                                                  // update rate in Hz
+        chrono::ChFrame<double>(driver_eye, driver_view_direction),  // offset pose
+        image_width,                                                 // image width
+        image_height,                                                // image height
         cam_fov,
         super_samples);  // fov, lag, exposure
     cam2->SetName("Camera Sensor");
@@ -372,7 +445,7 @@ int main(int argc, char* argv[]) {
     // ---------------
     // Simulate system
     // ---------------
-    float orbit_radius = 15.f;
+    float orbit_radius = 1000.f;
     float orbit_rate = .5;
 
     auto t0 = high_resolution_clock::now();
@@ -388,12 +461,12 @@ int main(int argc, char* argv[]) {
             break;
 
         cam->SetOffsetPose(
-            chrono::ChFrame<double>({-orbit_radius * cos(time * orbit_rate), -orbit_radius * sin(time * orbit_rate), 3},
-                                    Q_from_AngAxis(time * orbit_rate, {0, 0, 1})));
+            chrono::ChFrame<double>({-orbit_radius * cos(time * orbit_rate), -orbit_radius * sin(time * orbit_rate),  orbit_radius/5.0},
+                                    Q_from_AngAxis(time * orbit_rate, {0, 0,1})));
 
         // Collect output data from modules (for inter-module communication)
         ChDriver::Inputs driver_inputs;
-        if (driver_mode==AUTONOMOUS)
+        if (driver_mode == AUTONOMOUS)
             driver_inputs = PFdriver->GetInputs();
         else
             driver_inputs = IGdriver->GetInputs();
@@ -411,7 +484,7 @@ int main(int argc, char* argv[]) {
         }
 
         // Update modules (process inputs from other modules)
-        if (driver_mode==AUTONOMOUS)
+        if (driver_mode == AUTONOMOUS)
             PFdriver->Synchronize(time);
         else
             IGdriver->Synchronize(time);
@@ -423,25 +496,27 @@ int main(int argc, char* argv[]) {
 #endif
         // Advance simulation for one timestep for all modules
         double step = step_size;
-        
-        if (driver_mode==AUTONOMOUS)
+
+        if (driver_mode == AUTONOMOUS)
             PFdriver->Advance(step);
         else
             IGdriver->Advance(step);
         terrain.Advance(step);
         vehicle.Advance(step);
         app.Advance(step_size);
-        if (step_number % int(1 /(20 * step_size)) == 0) {
-            app.BeginScene(true, true, irr::video::SColor(255, 140, 161, 192));
-            ///irrlicht::tools::drawSegment(app.GetVideoDriver(), v1, v2, video::SColor(255, 80, 0, 0), false);
-            app.GetDevice()->getVideoDriver()->draw2DImage(app.GetDevice()->getVideoDriver()->getTexture((demo_data_path + "/miscellaneous/Speedometer.png").c_str()),
+        if (step_number % int(1 / (60 * step_size)) == 0) {
+            /// irrlicht::tools::drawSegment(app.GetVideoDriver(), v1, v2, video::SColor(255, 80, 0, 0), false);
+            app.GetDevice()->getVideoDriver()->draw2DImage(
+                app.GetDevice()->getVideoDriver()->getTexture(
+                    (demo_data_path + "/miscellaneous/Speedometer.png").c_str()),
                 irr::core::position2d<irr::s32>(0, 0));
+            /*app.GetDevice()->getVideoDriver()->draw2DImage(app.GetDevice()->getVideoDriver()->getTexture((demo_data_path
+               + "/miscellaneous/Needle.png").c_str()), irr::core::position2d<irr::s32>(200, 200));*/
             double speed_mph = vehicle.GetVehicleSpeedCOM() * 2.23694;
-            double theta = ((270/140)*speed_mph)*(CH_C_PI/180);
+            double theta = ((270 / 140) * speed_mph) * (CH_C_PI / 180);
             app.GetDevice()->getVideoDriver()->draw2DLine(
-                    sm_center + irr::core::position2d<irr::s32>(-sm_needle*sin(theta),sm_needle*cos(theta)),
-					sm_center,
-					irr::video::SColor (255,255,0,0));
+                sm_center + irr::core::position2d<irr::s32>(-sm_needle * sin(theta), sm_needle * cos(theta)), sm_center,
+                irr::video::SColor(255, 255, 0, 0));
             app.GetDevice()->getVideoDriver()->endScene();
         }
 
@@ -468,11 +543,7 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-
-void customButtonCallback(){
+void customButtonCallback() {
     std::cout << "Button Callback Invoked";
-    if(driver_mode == AUTONOMOUS)
-        driver_mode = HUMAN;
-    else if(driver_mode == HUMAN)
-        driver_mode = AUTONOMOUS;
+    driver_mode = HUMAN;
 }
